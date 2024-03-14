a30923b84f36655536c260c25b06a69beea017bd
carbon1423 <bockenhauercarson@gmail.com>
2024-03-14 16:31:54 -0500

    Comments of the individual piece classes methods

--- Chess/src/main/pieces/Knight.java
+++ Chess/src/main/pieces/Knight.java
@@ -8,13 +8,24 @@ public class Knight {
     private int row;
     private int col;
     private boolean isBlack;
-    
+    /**
+     * Constructor.
+     * @param row   The row of the knight.
+     * @param col   The column of the knight .
+     * @param isBlack   The color of the knight.
+     */
     public Knight(int row, int col, boolean isBlack) {
         this.row = row;
         this.col = col;
         this.isBlack = isBlack;
     }
-	
+    /**
+     * Verifies that the move is legal
+     * @param board the board that the knight is on
+     * @param endRow The end row to check
+     * @param endCol the end column to check
+     * @return returns a boolean true if the move is legal and false if the move is illegal
+     */
 	public boolean isMoveLegal(Board board, int endRow, int endCol) {
         if(col == endCol-1 || col == endCol+1) {
         	if(row == endRow-2 || row == endRow+2) {
--- Chess/src/main/pieces/Queen.java
+++ Chess/src/main/pieces/Queen.java
@@ -8,13 +8,24 @@ public class Queen {
     private int row;
     private int col;
     private boolean isBlack;
-    
+    /**
+     * Constructor.
+     * @param row   The row of the queen.
+     * @param col   The column of the queen.
+     * @param isBlack   The color of the queen.
+     */
     public Queen(int row, int col, boolean isBlack) {
         this.row = row;
         this.col = col;
         this.isBlack = isBlack;
     }
-
+    /**
+     * Verifies that the move is legal
+     * @param board the board that the queen is on
+     * @param endRow The end row to check
+     * @param endCol the end column to check
+     * @return returns a boolean true if the move is legal and false if the move is illegal
+     */
 	public boolean isMoveLegal(Board board, int endRow, int endCol) {
 		boolean legal = board.verifyVertical(row, col, endRow, endCol) || board.verifyHorizontal(row, col, endRow, endCol);
 		return legal || board.verifyDiagonal(row, col, endRow, endCol);
